3979ce47551aa7138fd7193e086dddc7f8ce63cb
Jay Prakash <50571500+jayprakash02@users.noreply.github.com>
2020-08-02 01:54:15 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -127,6 +127,6 @@ COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS =[
-        os.path.join(BASE_DIR,'main/static'),
+        os.path.join(BASE_DIR,'blogView/static'),
 ] 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'

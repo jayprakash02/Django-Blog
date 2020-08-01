06b2681d45870692505e04fbda9943675d83b0f5
Jay Prakash <50571500+jayprakash02@users.noreply.github.com>
2020-08-02 00:42:49 +0530

    deploy 1

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: guicorn CMS.wsgi --log-file -
--- blog/settings.py
+++ blog/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6eli_9(9a4ng$dfm4=r!tohv8j-e!#8=m916@n)&)o^^0(q4vv'
-
+# SECRET_KEY = '6eli_9(9a4ng$dfm4=r!tohv8j-e!#8=m916@n)&)o^^0(q4vv'
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://djblogstories.herokuapp.com/','127.0.0.1']
 
 
 # Application definition
@@ -76,10 +76,9 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default':  dj_database_url.config(
+        default=config('DATABASE_URL')
+    )   
 }
 
 
@@ -119,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-MEDIA_URL='/media/'
-
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR,'static/')
+STATICFILES_DIRS =[
+        os.path.join(BASE_DIR,'static'),
+] 
+STATICFILES_STORAGE = 'whitenoise.django.GzipMainfestStaticFilesStorage'
